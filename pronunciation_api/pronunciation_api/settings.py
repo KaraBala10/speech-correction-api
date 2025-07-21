497ced8a29a746d9dba9201b19ea784ffce3d0bf
KaraBala10 <mohammad.karabala@gmail.com>
2025-07-21 20:14:03 +0300

    fix: add authentication api

--- pronunciation_api/pronunciation_api/settings.py
+++ pronunciation_api/pronunciation_api/settings.py
@@ -80,7 +80,10 @@ TEMPLATES = [
 REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": [
         "rest_framework.renderers.JSONRenderer",
-    ]
+    ],
+    "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.TokenAuthentication",
+    ],
 }
 
 
